a285416cb4a2c0ae5c3525621d1fb7c20ce83b3b
Roman <krusman99@gmail.com>
2021-05-18 00:42:11 +0300

    Added Ability to change profile picture and profile password in Settings.java

--- app/src/main/java/com/example/squff2/Settings.java
+++ app/src/main/java/com/example/squff2/Settings.java
@@ -1,21 +1,35 @@
 package com.example.squff2;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.r0adkll.slidr.Slidr;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -25,11 +39,12 @@ import static com.example.squff2.LoginPage.MyPREFERENCES;
 
 public class Settings extends AppCompatActivity {
 
-    ImageView logout_btn;
+    ImageView logout_btn, profile_pic;
     Button changePass;
     TextView username;
     FirebaseAuth mAuth;
     SharedPreferences sharedpreferences;
+    StorageReference storageReference;
 
 
     @Override
@@ -38,14 +53,23 @@ public class Settings extends AppCompatActivity {
         setContentView(R.layout.activity_settings);
 
         username = findViewById(R.id.username);
+        profile_pic = findViewById(R.id.profile_pic);
 
+
+        storageReference = FirebaseStorage.getInstance().getReference();
         mAuth = FirebaseAuth.getInstance();
+        StorageReference profileRef = storageReference.child("users/"+mAuth.getCurrentUser().getUid()+"profile.jpg");
+        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).into(profile_pic);
+            }
+        });
 
 
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         String email = sharedpreferences.getString("Username", null);
         username.setText(email);
-
 
 
 
@@ -66,15 +90,95 @@ public class Settings extends AppCompatActivity {
         });
 
 
-        changePass.setOnClickListener(new View.OnClickListener() {
+
+        profile_pic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent open = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(open, 1000);
             }
         });
 
 
 
+        changePass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText resetMail = new EditText(v.getContext());
+                    AlertDialog.Builder passwordResetDialog = new AlertDialog.Builder(v.getContext());
+                    passwordResetDialog.setTitle("Do you want to reset password?");
+                    passwordResetDialog.setMessage("Enter your email, where you want receive reset link.");
+                    passwordResetDialog.setView(resetMail);
+
+                    passwordResetDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                            String mail = resetMail.getText().toString();
+                            mAuth.sendPasswordResetEmail(mail).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toasty.success(Settings.this, "Reset Link was sent.", Toasty.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toasty.warning(Settings.this, "Error! Reset Link was not sent!" + e.getMessage(), Toasty.LENGTH_SHORT).show();
+                                }
+                            });
+
+                        }
+                    });
+
+                    passwordResetDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+
+                        }
+                    });
+
+                passwordResetDialog.create().show();
+            };
+        });
+
+
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1000){
+            if(resultCode == Activity.RESULT_OK){
+                Uri imageUri = data.getData();
+                profile_pic.setImageURI(imageUri);
+                uploadImageToFirebase(imageUri);
+            }
+
+        }
+    }
+
+    private void uploadImageToFirebase(Uri imageUri) {
+        StorageReference fileRef = storageReference.child("users/"+mAuth.getCurrentUser().getUid()+"profile.jpg");
+        Log.d(mAuth.getCurrentUser().getUid(),"Hui");
+        fileRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                fileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        Picasso.get().load(uri).into(profile_pic);
+                    }
+                });
+                Toasty.success(Settings.this, "Image Uploaded.", Toasty.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toasty.warning(Settings.this, "Something went wrong.", Toasty.LENGTH_LONG).show();
+            }
+        });
     }
 
     private boolean isNetworkAvailable() {
